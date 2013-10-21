2a6ee8adb72d2b3c7d2349e122ba431b55f94630
jbowkett <james_bowkett@lyagushka.co.uk>
2013-10-21 22:04:52 +0100

    renames file

--- lib/prepend_filedate_to_filename.rb
+++ lib/prepend_filedate_to_filename.rb
@@ -1,3 +1,4 @@
+require 'find'
 
 class PrependFiledateToFilename
   def format(date)
@@ -5,6 +6,22 @@ class PrependFiledateToFilename
   end
 
 
+  def apply_to_all(start_dir)
+    Dir.foreach(start_dir) do |fname|
+      input_file = File.new("#{start_dir}/#{fname}")
+      next if File.directory?fname
+      new_filename = "#{format(File.mtime(input_file))}_#{fname}"
 
+      puts " rename #{fname} => #{new_filename}"
 
+      File.rename(input_file, "#{start_dir}/#{new_filename}")
+    end
+  end
 end
+
+if ARGV.length != 1
+  puts 'Usage: PrependFiledateToFilename <start dir>'
+  exit -1
+end
+
+PrependFiledateToFilename.new.apply_to_all(ARGV[0])
