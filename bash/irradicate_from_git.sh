3028038d5dea3ed39bfb9c75dab1cb1b5d71861b
jbowkett <james_bowkett@lyagushka.co.uk>
2013-11-11 09:40:04 +0000

    updated with github ref

--- bash/irradicate_from_git.sh
+++ bash/irradicate_from_git.sh
@@ -2,6 +2,7 @@
 
 # Deletes a file/path from all the check in history in git.
 # I believe it only works on the current branch, but use it with caution!!
+# See also: https://help.github.com/articles/remove-sensitive-data
 
 export file_path_to_delete=${1}
 
