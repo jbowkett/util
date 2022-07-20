fd94466ac0a0ec323a7974d8bbd5341045f42c94
jbowkett <james@bowkett.info>
2022-07-20 18:28:39 +0000

    new script to sort photos from current lot into modified-date file folders

---
+++ bash/sort-photos.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+usage (){
+  printf "\n\n usage : sort-photos.sh <path to src files> <path to destination files>\n\n"
+
+}
+
+if [ "$#" -ne 2 ]; then
+  usage
+  exit 255
+fi
+
+
+src=$1
+dest=$2
+
+for file in $src
+do
+
+  if [ -f "$file" ]
+    then
+    echo "processing file $file"
+    year=`date -r ${file} "+%Y"`
+    month=`date -r ${file} "+%b"`
+    dest_path="${dest}/${year}/${month}/"
+    echo "making path ${dest_path}..."
+    mkdir -p ${dest_path} 
+    mv_cmd="mv ${file} ${dest}/${year}/${month}/"
+    `${mv_cmd}`
+  fi
+
+done
+
